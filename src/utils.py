6831306bdde9466151d497c370ccd9a3c1c34c33
Mohamedarsath26 <mdarsath2606@gmail.com>
2025-04-05 22:37:39 +0530

    model_trainer is successfully added

--- src/utils.py
+++ src/utils.py
@@ -4,8 +4,7 @@ import sys
 import numpy as np
 import pandas as pd
 import dill
-
-
+from sklearn.metrics import r2_score
 
 from src.exception import CustomException
 
@@ -17,3 +16,25 @@ def save_object(file_path, obj):
             dill.dump(obj, file_obj)
     except Exception as e:
         raise CustomException(e, sys)
+    
+
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    try:
+        report = {}
+
+        for name, model in models.items():
+            model.fit(X_train, y_train)
+
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            train_model_r2score = r2_score(y_train, y_train_pred)
+            test_model_r2score = r2_score(y_test, y_test_pred)
+
+            report[name] = test_model_r2score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
